d5fe62269158329e973d916ad695e173d257867b
SivkimHENG <hengsivkim14@gmail.com>
2024-05-25 06:05:37 -0500

    all css design

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
 const {src, dest, watch ,series} = require("gulp");
 const sass = require("gulp-sass")(require("sass"))
-
+const purgecss = require('gulp-purgecss')
 function buildStyle() {
 
 return src('Refy/**/*.scss' )
 .pipe(sass())
+.pipe(purgecss({content: ['*.html']}))
 .pipe(dest('css'))
 
 
 }
 
 function watchTask() {
-  watch(['Refy/**/*.css'], buildStyle)
+  watch(['Refy/**/*.css','*.html'], buildStyle)
 }
 
 
